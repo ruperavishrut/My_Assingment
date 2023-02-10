8b3b3b884bc0503aa96cd662a9a77e25ba1451e1
Vishrut Rupera <vishrutrupera@gmail.com>
2023-02-10 15:57:00 +0530

    My_Assign

--- Ass27_Tringle345.java
+++ Ass27_Tringle345.java
@@ -14,21 +14,65 @@ class Traingle
 	public void traingle3()
 	{
 		Scanner sc=new Scanner(System.in);
-		System.out.println("This Is 3 Side Traingle : ");
-		System.out.println("Enter First Value For 3 Side Traingle: ");
-		int t1=sc.nextInt();
+		System.out.println("This Is 3 Side Traingle.... ");
+		System.out.println(" ");
+		System.out.println("Enter Value For 3 Side Traingle: ");
 		
-		System.out.println("Enter Second Value For 3 Side Traingle: ");
-		int t2=sc.nextInt();
+		double t1=sc.nextDouble();
+		double t2=sc.nextDouble();
+		double t3=sc.nextDouble();
 		
-		System.out.println("The 3 side of Traingle is : "+(t1*t2)/2);
+		double peramiter =t1+t2+t3;
+		
+		System.out.println("The Perimiter of The Traingle IS >>> "+peramiter);
+		
+		double s=peramiter/2;
+		
+		double area=Math.sqrt(s*(s-t1)*(s-t2)*(s-t3));
+		
+		System.out.println("The Area of The Taingle is >>> "+area);
 	}
 	public void traingle4()
 	{
+		Scanner sc=new Scanner(System.in);
+		System.out.println(" ");
+		System.out.println("Enter Value For 4 Side Traingle: ");
+		
+		double t1=sc.nextDouble();
+		double t2=sc.nextDouble();
+		double t3=sc.nextDouble();
+		double t4=sc.nextDouble();
+		
+		double peramiter =t1+t2+t3+t4;
+		System.out.println("The Perimiter of The 4 side Traingle IS >>> "+peramiter);
+		
+		double l1=(t1+t2)/2;
+		double w1=(t3+t4)/2;
+		
+		System.out.println("Area Of 4 side Traingle IS : "+(l1*w1));
 		
 	}
 	public void traingle5()
 	{
+		Scanner sc=new Scanner(System.in);
+		System.out.println(" ");
+		System.out.println("Enter Value For 5 Side Traingle: ");
+		
+		double t1=sc.nextDouble();//5
+		double t2=sc.nextDouble();//4
+		double t3=sc.nextDouble();//8
+		double t4=sc.nextDouble();//4
+		double t5=sc.nextDouble();//5
+		
+		double peramiter = t1+t3+t5;
+		double sp=peramiter/2;
+		double area = Math.sqrt(sp*(sp-t1)*(sp-t3)*(sp-t5));
+		//System.out.println(area);
+		
+		double area2=t2*t3;
+		//System.out.println(area2);	
+		System.out.println("Area Of 5 Side Traingle : "+(area+area2));
+		
 		
 	}
 }
